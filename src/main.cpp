360d0f467d733176b4e8ac455762aa20780a0aad
joaquind <joaquind@students.21-school.ru>
2023-10-26 20:32:55 +0300

    Add makefile next time

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,18 @@
 #include <window.h>
 
 int main() {
-  s21::Finder finder;
-  s21::MazeGenerator mg;
-  s21::Controller controller(&finder, &mg);
-  s21::Facade facade(&controller);
-  s21::Window w(&facade);
-  w.Start();
-  return 0;
+    std::vector <s21::Button> buttons_{};
+    std::vector <s21::TextTables> text_labels_{};
+
+    s21::Field field;
+    s21::Finder finder;
+    s21::MazeGenerator mg;
+    s21::Controller controller(&finder, &mg);
+
+    s21::Facade facade(&controller, &buttons_, &text_labels_, &field);
+
+    s21::Window w(&facade);
+
+    w.Start();
+    return 0;
 }
